4e3469d2cde208b44da55c81812f5efbeb99afe0
Anshuman Rath <anshumanrath030@gmail.com>
2024-04-21 01:18:29 +0530

    .

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -30,4 +30,6 @@ urlpatterns = [
     
     path('sayhi', cntviews.hipage,name='sayhi'),
     path('contactme', cntviews.contactme,name='contactme'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
