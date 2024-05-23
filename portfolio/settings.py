33a6ffad0cedcffcd251a5c2994d30067f7a4adb
Anshuman Rath <anshumanrath030@gmail.com>
2024-05-23 11:42:17 +0530

    trying

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -91,16 +91,24 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER':'postgres',
-        'PASSWORD':'qjyQwYYXndsZzJFxmFUpuqrZDPBhPPTw',
-        'HOST':'roundhouse.proxy.rlwy.net',
-        'PORT':'20092',
-    }
+        'NAME': 'Portfolio',
+        'USER':'ANISH',
+        'PASSWORD':'rFaih5EKD8Wf',
+        'HOST':'ep-calm-shadow-a1p26sgj.ap-southeast-1.aws.neon.tech',
+        'PORT':'5432',
+    },
+    # 'railwaydb': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'railway',
+    #     'USER':'postgres',
+    #     'PASSWORD':'qjyQwYYXndsZzJFxmFUpuqrZDPBhPPTw',
+    #     'HOST':'roundhouse.proxy.rlwy.net',
+    #     'PORT':'20092',
+    # },
+    
 }
 from django.core.cache import cache
 
-# Clear the entire cache
 cache.clear()
 
 # DATABASES = {
@@ -147,12 +155,8 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles_build','static')
-
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
