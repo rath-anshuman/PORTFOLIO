f2fa5f9d6ca0e2136c918b8f08509a3c9e3e51a8
Anshuman Rath <anshumanrath030@gmail.com>
2024-08-18 00:12:01 +0530

    .

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -179,6 +179,7 @@ PWA_APP_SCOPE = '/'
 PWA_APP_ORIENTATION = 'any'
 PWA_APP_START_URL = '/'
 PWA_APP_STATUS_BAR_COLOR = 'default'
+
 PWA_APP_ICONS = [
     {
         'src': '/static/data/intro/mf-avatar.svg',
@@ -199,17 +200,4 @@ PWA_APP_SPLASH_SCREEN = [
 ]
 PWA_APP_DIR = 'ltr'
 PWA_APP_LANG = 'en-US'
-PWA_APP_SHORTCUTS = [
-    {
-        'name': 'Shortcut',
-        'url': '/target',
-        'description': 'Shortcut to a page in my application'
-    }
-]
-PWA_APP_SCREENSHOTS = [
-    {
-      'src': '/static/data/intro/mf-avatar.svg',
-      'sizes': '750x1334',
-      "type": "image/png"
-    }
-]
+
