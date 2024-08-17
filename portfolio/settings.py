f5435ccf86a48098e9f4991f933f17830729b0ba
Anshuman Rath <anshumanrath030@gmail.com>
2024-08-17 23:10:01 +0530

    .

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'projectpg.apps.ProjectpgConfig',
     'contactme.apps.ContactmeConfig',
     'cloudinary',
+    'pwa',
 ]
 
 MIDDLEWARE = [
@@ -140,7 +141,11 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles_build','static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
@@ -158,3 +163,53 @@ cloudinary.config(
   api_secret = "Rqx8duoNINh1r1xX0hNo9qRMUHo" 
   
 )
+
+
+
+
+#-----PWA---------
+
+
+PWA_APP_NAME = 'Anshuman Rath'
+PWA_APP_DESCRIPTION = "http://anshumanrath.in/"
+PWA_APP_THEME_COLOR = '#1c82ff'
+PWA_APP_BACKGROUND_COLOR = '#ffffff'
+PWA_APP_DISPLAY = 'standalone'
+PWA_APP_SCOPE = '/'
+PWA_APP_ORIENTATION = 'any'
+PWA_APP_START_URL = '/'
+PWA_APP_STATUS_BAR_COLOR = 'default'
+PWA_APP_ICONS = [
+    {
+        'src': '/static/data/intro/mf-avatar.svg',
+        'sizes': '160x160'
+    }
+]
+PWA_APP_ICONS_APPLE = [
+    {
+        'src': '/static/data/intro/mf-avatar.svg',
+        'sizes': '160x160'
+    }
+]
+PWA_APP_SPLASH_SCREEN = [
+    {
+        'src': '/static/data/intro/mf-avatar.svg',
+        'media': '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)'
+    }
+]
+PWA_APP_DIR = 'ltr'
+PWA_APP_LANG = 'en-US'
+PWA_APP_SHORTCUTS = [
+    {
+        'name': 'Shortcut',
+        'url': '/target',
+        'description': 'Shortcut to a page in my application'
+    }
+]
+PWA_APP_SCREENSHOTS = [
+    {
+      'src': '/static/data/intro/mf-avatar.svg',
+      'sizes': '750x1334',
+      "type": "image/png"
+    }
+]
